4d06e75ac7678d4d245708be4903016de0f336bd
Sara DAMHANE <sara.damhane@etu.univ-smb.fr>
2025-12-10 16:07:03 +0100

    Changement UCChoixPoissons (images, dispositions, boutons + radiobuttons +titre)

--- AquaSurvivor/UCChoixPoisson.xaml.cs
+++ AquaSurvivor/UCChoixPoisson.xaml.cs
@@ -24,5 +24,10 @@ namespace AquaSurvivor
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
